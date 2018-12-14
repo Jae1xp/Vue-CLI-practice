b754c38f503562626c64081f803ad1f616b77fe8
Jae Son <>
2018-12-14 17:54:07 -0500

    added submit and clear field functionality

--- vue-blog-post/blog-post/src/components/addBlog.vue
+++ vue-blog-post/blog-post/src/components/addBlog.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="add-blog">
     <h1>Add a New Blog Post</h1>
-    <form>
+    <!-- This if will hide the form if it has been submitted. Refer to the submit div on line 30 -->
+    <form v-if="!submitted">
       <label>Blog Title:</label>
       <!-- By using blog.title in the model, you can better organize your code by having a BLOG object -->
       <!-- .lazy just makes whatever input you type, not appear until you tab out of the input field -->
@@ -22,7 +23,14 @@
       <select v-model="blog.author">
         <option v-for="author in authors">{{author}}</option>
       </select>
+      <!-- .prevent is the = to prevent default -->
+      <button v-on:click.prevent="post">Add Post!</button> 
     </form>
+    <!-- If the form has been submitted, this message will show -->
+    <div v-if="!submitted">
+      <h2>Thanks for adding your post!!</h2>
+    </div>
+
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog Title: {{blog.title}}</p>
@@ -50,11 +58,24 @@ export default {
         categories: [],
         author: ''
       },
-      authors: ['Michelle Obama', 'J.K. Rowling', 'Randy Pausch', 'Kobe Bryant']
+      authors: ['Michelle Obama', 'J.K. Rowling', 'Randy Pausch', 'Kobe Bryant'],
+      // tracks whether the form has been submitted or not
+      submitted: false,
     }
   },
   methods: {
-    
+    post: function() {
+      // Can use $http only after installing vue resource
+      // Check this URL to see why we require BODY and USER ID
+      this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1,
+      }).then(function(data) {
+        console.log('this is the data', data);
+        this.submitted = true;
+      });
+    }
   }
 }
 </script>
