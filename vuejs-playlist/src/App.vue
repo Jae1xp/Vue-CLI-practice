38f85a295bb74cc4b3d52fe93f6429e2fa713281
Jae Son <>
2018-12-14 12:07:26 -0500

    working with form helper and SLOTS

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <!-- <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
     <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer v-bind:title="title"></app-footer>
+    <app-footer v-bind:title="title"></app-footer> -->
+    <form-helper >
+      <h2 slot="title">I am the slot title</h2>
+      <p slot="paragraph">I am the slot paragraph</p>
+    </form-helper>
   </div>
 </template>
 
@@ -10,12 +14,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Ninjas from './components/Ninjas';
+import formHelperVue from './components/formHelper.vue';
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    // 'app-header': Header,
+    // 'app-footer': Footer,
+    // 'app-ninjas': Ninjas
+    'form-helper': formHelperVue
   },
   data () {
     return {
---
+++ vuejs-playlist/src/components/formHelper.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <h1>I am the form helper</h1>
+    <slot name="paragraph"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
